8abcbcc83096429b137a64b3a4eef8fc273d0afc
Yash Hegde <yash4success@gmail.com>
2024-03-09 18:54:59 +0530

    schema

--- src/index.ts
+++ src/index.ts
@@ -30,5 +30,9 @@ app.get('/api/v1/blog/bulk', (c) => {
   return c.text('Hello Hono!')
 })
 
+/* postgresql://Yh010:uR6ISxoWidy8@ep-still-cherry-a5ycjgb4.us-east-2.aws.neon.tech/medium?sslmode=require */
+
+/* prisma accelerate key: DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiNDYzOTNmYzEtYzUzZC00NDY0LTlmNjQtNWI2M2FjNzFlYTQzIiwidGVuYW50X2lkIjoiY2RhYjYwMTQ0Njc2NWZkOTQzYzBiM2U5ZGE5YTZhMThhNTQyNTE4NTlmMTlkZTM0ZjczOTgxODI4NWY5ZjY1NCIsImludGVybmFsX3NlY3JldCI6IjllNTMzYzAyLTM5ZTgtNDM3YS05YzRkLTEwOWQyMTg1ZTNhNiJ9.zKYuc79zrSpi43_AkjagQWoSz7yOqF399E0NpSMhR0I"*/
+
 
 export default app
